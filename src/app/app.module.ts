f9e2a7a749127af2008b9498a7749ffddbd2627d
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-05-25 19:49:47 +0000

    sdfsdfsd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
-import { DefaultComponent } from './Components/default/default.component';
 import { ListaComponent } from './Components/lista/lista.component';
 import { PlayComponent } from './Components/play/play.component';
+import { DefaultComponent } from './Components/default/default.component';
 
 @NgModule({
   declarations: [
