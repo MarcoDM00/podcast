8a19ae81f93f7aa9b949c9fc6982a6d3f76a459d
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-05-25 16:51:33 +0000

    finito routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routes';
+import { DefaultComponent } from './Components/default/default.component';
+import { ListaComponent } from './Components/lista/lista.component';
+import { PlayComponent } from './Components/play/play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    ListaComponent,
+    PlayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
